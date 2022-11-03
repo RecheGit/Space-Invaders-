0eab62f3995998e43c9b25f75e3bfe2b51c3c63a
Kepa <githubkepa@gmail.com>
2022-11-03 17:52:08 +0100

    Bordes marcados y caida de bolas

--- Space_Invaders.py
+++ Space_Invaders.py
@@ -41,6 +41,7 @@ def movimiento_teclado(event,x_speed,y_speed):
 listacord = []
 i=0
 while i<= 20:
+    #Tenemos que incluir x e y porque sino solo nos salen los puntos en una recta
     obj_x = random.randint(0,800)
     obj_y = random.randint(0,500)
     pygame.draw.circle(screen,BLACK, (obj_x,obj_y), 2)
@@ -60,17 +61,28 @@ def caida_objetos():
 #Bucle principal
 
 
-while True:
+fin_juego=False
+while not fin_juego:
     for evento in pygame.event.get():
         if evento.type == pygame.QUIT:
             sys.exit()
         x_speed=movimiento_teclado(evento,x_speed,y_speed)[0]
         y_speed=movimiento_teclado(evento,x_speed,y_speed)[1]
-        print(coord_x)
+        #print(coord_x)
+        print(coord_y)
     screen.fill(WHITE)
     caida_objetos()
     coord_x += x_speed
-    coord_y += y_speed           
+    coord_y += y_speed
+    if coord_x > 702 :#limites del cubo con los bordes laterales
+        coord_x=702
+    elif coord_x <= 0:
+        coord_x=0           
+    if coord_y > 400 :#limites del cubo con los bordes laterales
+        coord_y=400
+    elif coord_y <= -2:
+        coord_y=-2           
+    
     pygame.draw.rect(screen, RED, (coord_x, coord_y, 100, 100))
     
     pygame.display.flip()
