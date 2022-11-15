80e29130c679bc91734e4232fd6c82154e3e9bc3
Kepa <githubkepa@gmail.com>
2022-11-15 18:48:35 +0100

    imagenes

--- main.py
+++ main.py
@@ -16,14 +16,13 @@ class Meteorito(pygame.sprite.Sprite):
 	def __init__(self):
 		super().__init__()
 		self.image = pygame.image.load("imagenes/meteorito.png").convert()
-        #self.image.transform.scale(play_img, (width-300, height-80))
 		self.image.set_colorkey(BLACK)
 		self.rect = self.image.get_rect()#Guardar posición
         
 
 	def update(self,choque):
 		if choque==True:
-			self.rect.y = 600	  
+			self.kill()	  
 		self.rect.y +=1        
 		if self.rect.y > 600 :
 			self.rect.y = -5
@@ -36,13 +35,10 @@ class Meteorito(pygame.sprite.Sprite):
 class Nave(pygame.sprite.Sprite):
 	def __init__(self):
 		super().__init__()
-		self.image = pygame.image.load("imagenes/nave.png").convert()
+		self.image = pygame.image.load("imagenes/playerN.png").convert()
 		self.image.set_colorkey(BLACK)
 		self.rect = self.image.get_rect()
-
-
-
-
+ 
 pygame.init()
 
 size = (1000, 600)
@@ -52,17 +48,15 @@ background = pygame.image.load("imagenes/backgala.webp").convert()
 background_menuInicio = pygame.image.load("imagenes/galaxy.webp").convert()
 play_img= pygame.image.load("imagenes/play.webp").convert_alpha()
 width = play_img.get_rect().width
-print(width)
 height = play_img.get_rect().height
-print(height)
-play_img = pygame.transform.scale(play_img, (width-300, height-80))
+play_img = pygame.transform.scale(play_img, (width-500, height-100))
 
 
 #bo
 #screen.blit(play_img,(300,500) )
 
 fin_juego=False
-VIDA=3
+VIDA=1
 
 
 #Coordenadas de la nave al inicio
@@ -107,6 +101,8 @@ estado_menu = "menuInicial"
 
 
 fuente=pygame.font.SysFont("arialblack",100)
+fuente2=pygame.font.SysFont("arialblack",50)
+
 
 
 
@@ -114,22 +110,22 @@ def movimiento_teclado(event,x_speed,y_speed):
 
     if event.type == pygame.KEYDOWN:
 
-        if event.key == pygame.K_LEFT:
+        if event.key == pygame.K_a:
             x_speed = -3
-        if event.key == pygame.K_RIGHT:
+        if event.key == pygame.K_d:
             x_speed = 3
-        if event.key == pygame.K_UP:
+        if event.key == pygame.K_w:
             y_speed = -3
-        if event.key == pygame.K_DOWN:
+        if event.key == pygame.K_s:
             y_speed = 3
     if event.type == pygame.KEYUP:
-        if event.key == pygame.K_LEFT:
+        if event.key == pygame.K_a:
             x_speed = 0
-        if event.key == pygame.K_RIGHT:
+        if event.key == pygame.K_d:
             x_speed = 0
-        if event.key == pygame.K_UP:
+        if event.key == pygame.K_w:
             y_speed = 0
-        if event.key == pygame.K_DOWN:
+        if event.key == pygame.K_s:
             y_speed = 0
     
 
@@ -149,44 +145,73 @@ crear_meteoritos(avisoTiempoSinChoque)
 
 #load button images
 start_img = pygame.image.load('imagenes/play.webp').convert_alpha()
-#exit_img = pygame.image.load('exit_btn.png').convert_alpha()
+controles_img = pygame.image.load('imagenes/ajustes.webp').convert_alpha()
+return_img = pygame.image.load('imagenes/return.webp').convert_alpha()
+exit_img = pygame.image.load('imagenes/x.webp').convert_alpha()
+tick_img = pygame.image.load('imagenes/tick.webp').convert_alpha()
+
 
 #create button instances
-start_button = button.Button(100, 200, start_img, 0.8)
-#exit_button = button.Button(450, 200, exit_img, 0.8)
+start_button = button.Button(480, 200, start_img, 0.05)
+ajustes_button = button.Button(475, 275, controles_img, 0.05)
+return_button = button.Button(930, 526, return_img, 0.05)
+exit_button = button.Button(475, 526, exit_img, 0.05)
+tick_button = button.Button(600, 526, tick_img, 0.05)
+
 
 game_mode= "menuInicio"
 click=False
 while not fin_juego:
+    if game_mode== "controles":
+        screen.blit(background_menuInicio, [0, 0])
+        screen.blit(fuente.render("CONTROLES ", True,WHITE ), (300, 50))
+        screen.blit(fuente2.render("Desplazamiento Derecha: d", True,WHITE ), (5, 130))
+        screen.blit(fuente2.render("Desplazamiento Izquierda: a ", True,WHITE ), (5, 200))
+        screen.blit(fuente2.render("Desplazamiento Frontal: w ", True,WHITE ), (5, 270))
+        screen.blit(fuente2.render("Desplazamiento Trasero: s ", True,WHITE ), (5, 330))
+        screen.blit(fuente2.render("Menu de Pausa: p ", True,WHITE ), (5, 400))
 
-    while game_mode == "menuInicio":
-        #print("HOLAA")
+
+        if return_button.draw(screen):
+            game_mode="menuInicio"
+        for evento in pygame.event.get():
+            if evento.type== pygame.QUIT:
+                sys.exit()
+        pygame.display.update()
+
+    
+
+    if game_mode == "menuInicio":
         screen.blit(background_menuInicio, [0, 0])
         screen.blit(fuente.render("CHOQUE ESPACIAL", True,WHITE ), (150, 50))
         if start_button.draw(screen):
-            print("START")
             game_mode="jugando"
+        if ajustes_button.draw(screen):
+            game_mode="controles"
         for evento in pygame.event.get():
             if evento.type== pygame.QUIT:
-                fin_juego=True
+                sys.exit()
+
         pygame.display.update()
 
 
-    if game_mode == "partida_pausada": 
-       # fuente.render(text,True, text_col)  
+    elif game_mode == "partida_pausada": 
         reactivar = False
         while reactivar == False:
             screen.blit(fuente.render("JUEGO EN PAUSA", True,WHITE ), (160, 250))
             pygame.display.flip()
             for evento in pygame.event.get():
+                if evento.type== pygame.QUIT:
+                    sys.exit()
                 if evento.type == pygame.KEYDOWN:
                     if evento.key == pygame.K_p:
                         game_mode = "jugando"
                         reactivar=True
                         pygame.display.flip()
-                        #print("frantxu30")
-
+                        
     elif game_mode == "jugando":
+       
+        
         if tiempoSinChoque == 1000:#subir de dificultad periodicamente
             avisoTiempoSinChoque=True
             crear_meteoritos(avisoTiempoSinChoque)
@@ -200,7 +225,6 @@ while not fin_juego:
 
                 if evento.key == pygame.K_p:
                     game_mode = "partida_pausada"
-                    #texto_menu("Partida Pausada", fuente, WHITE, 160, 250)
 
             x_speed=movimiento_teclado(evento,x_speed,y_speed)[0]
             y_speed=movimiento_teclado(evento,x_speed,y_speed)[1]
@@ -213,19 +237,19 @@ while not fin_juego:
         nave.rect.y = coord_y
         numcolisiones=pygame.sprite.spritecollide(nave, ListaMeteoritos, True)
         for colisiones in numcolisiones:
+
             Choque =True
             avisoTiempoSinChoque=False
             TodosSprite.update(Choque)
-            #time.sleep(0.25)
+            crear_meteoritos(avisoTiempoSinChoque)
             coord_x = 500
             coord_y = 500
-            time.sleep(0.10)
-            #crear_meteoritos(avisoTiempoSinChoque)
+            time.sleep(0.10)#Nose si añadirlo
             VIDA -=1 
             print("LE QUEDAN " + str(VIDA)+" VIDAS")    
             if VIDA == 0:
-                print("GAME OVER")
-                fin_juego=True
+                game_mode="menu_fin"
+                #fin_juego=True
         Choque=False
         TodosSprite.draw(screen)
 
@@ -245,6 +269,26 @@ while not fin_juego:
         
 
         clock.tick(170) #Frames por segundo  
+    elif game_mode == "menu_fin":
+        screen.blit(background_menuInicio, [0, 0])
+        screen.blit(fuente.render("GAME OVER", True,WHITE ), (275, 50))
+        screen.blit(fuente.render("¿JUGAR OTRA VEZ?", True,WHITE ), (275, 400))
+        #MOSTAR RANKING?
+        if exit_button.draw(screen):
+            sys.exit()
+        if tick_button.draw(screen):
+            VIDA=1
+            coord_x = 500
+            coord_y = 500
+            x_speed=0
+            y_speed=0
+            game_mode="jugando"
+        for evento in pygame.event.get():
+            if evento.type== pygame.QUIT:
+                sys.exit()
+
+       # pygame.display.update()
+
 
     pygame.display.update()
 
@@ -254,7 +298,7 @@ while not fin_juego:
 #MEJORAS NECESARIAS
 
 #INCREMENTAR EL NIVEL EN FUNCION DEL TIEMPO
-#MINIMIZAR EL TAMAÑO DE LA NAVE
+
 #NUEVA FUNCIONALIDAD MENU FINAL
 
  #AÑADIR PARA PONER USUARIO
